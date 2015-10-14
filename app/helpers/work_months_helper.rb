03a8f9ffd4c96f5a3422f63d38cb96ff95c973fc
Weiyan Shao <lighteningman@gmail.com>
2015-10-14 18:38:45 +0200

    Hightlight weekends

--- app/helpers/work_months_helper.rb
+++ app/helpers/work_months_helper.rb
@@ -2,4 +2,10 @@ module WorkMonthsHelper
   def employees_select_options
     Employee.all.collect { |e| [ e.name, e.id ] }
   end
+
+  def weekend_class(day)
+    if day.sunday? || day.saturday?
+      "weekend"
+    end
+  end
 end
